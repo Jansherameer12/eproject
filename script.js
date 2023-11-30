351b87a9de7b378fc6f695b95a9b5da44fdebc17
Jansher Ameer <Jansherameer@gmail.com>
2023-11-30 10:14:57 +0500

    Files updated

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ Shery.mouseFollower();
 Shery.hoverWithMediaCircle(".hover-target" , {
     images: ["images/image1.jpg","images/image2.jpg","images/image3.jpg","images/image4.jpg" ]
   });
+
+
+
+
+  function Onclick(){
+    Swal.fire({
+      title: "Success!",
+      text: "The message sent successfully!",
+      icon: "success"
+    });
+  }
