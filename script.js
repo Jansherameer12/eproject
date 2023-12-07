7c80ca58f45a7f1dee6288998cc5b35e9c49fcfc
Jansher Ameer <Jansherameer@gmail.com>
2023-12-07 10:38:09 +0500

    Files updated

--- script.js
+++ script.js
@@ -37,3 +37,42 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+
+
+
+// Menu icon responsive
+
+var menu = document.getElementById('menu');
+var nav = document.getElementById('nav');
+var isMenuOpen = false; 
+menu.addEventListener('click', function () {
+    if (isMenuOpen) {
+        nav.style.left = "-100%"; 
+        menu.classList.remove('fa-close');
+        menu.classList.add('fa-bars');
+    } else {
+        nav.style.left = "0";
+        menu.classList.remove('fa-bars');
+        menu.classList.add('fa-close');
+    }
+    isMenuOpen = !isMenuOpen;
+});
+
+function cart() {
+  var number = Math.floor(Math.random() * 90) + 10;
+  Swal.fire({
+    title: "You have booked a ticket!",
+    text: `Your id is ${number + Date.now().toString()}`,
+    icon: "success"
+  });
+}
+
+
+
+function book() {
+  var cart = document.getElementById('cart');
+  cart.style.right = '0';
+  // window.addEventListener('click', function () {
+  //   cart.style.right = '-1000px';
+  // }); // Closing parenthesis was added here
+}
