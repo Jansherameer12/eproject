430b7d147fb4609257d717222c2a8beb5e4ae2ea
Jansher Ameer <Jansherameer@gmail.com>
2023-12-11 23:10:22 +0500

    New changes

--- script.js
+++ script.js
@@ -202,3 +202,21 @@ typewriter
     }
   }
   
+
+  // TOP BTN
+
+  window.onscroll = function() { scrollFunction() };
+
+  function scrollFunction() {
+    var btn = document.querySelector(".btntop");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  }
+
+  function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
